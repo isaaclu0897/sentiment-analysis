04d6c84c5775188005dfa3f436e271b1939e60d8
wei <t104306033@ntut.org.tw>
2018-03-27 22:37:10 +0800

    optimize code

--- crawler_yahoomovie.py
+++ crawler_yahoomovie.py
@@ -6,48 +6,34 @@ Created on Tue Mar 27 19:38:47 2018
 @author: wei
 """
 
-import requests 
-from bs4 import BeautifulSoup 
 import jieba
 from nbtool import change_page, parse_url
 
 url = "https://movies.yahoo.com.tw/movieinfo_review.html/id=6953?sort=update_ts&order=desc&page=1" 
-for i in range(3):# 這邊決定換頁換幾次 
-     
+item = []
+for i in range(1):# 這邊決定換頁換幾次 
     print(url)# 確定當前頁面 
-    res = requests.get(url) 
-    soup = BeautifulSoup(res.text, "lxml") 
-     
-    articles = [] 
-# comment_block = soup.select('.usercom_list') 
-# print(comment_block) 
-     
-    divs = soup.find_all('div', 'usercom_inner _c') 
-    for item in divs: #soup.select('#form_good1'):# :.string.text.text  
-        user = item.find('div', 'user_id unuser').text 
-        #print (user) 
-        scor = item.find('input',{'name':"score"})['value'] 
-        score = int(scor) 
-        #print (score) 
-# articles.append({'user':user, 
-# 'score':score,}) 
-        a = item.find_all('span') 
-        for comment in a: 
-            if comment.text == '': 
-                continue 
-            com = (comment.text).replace('\r\n', '')
-            coms_sep = jieba.cut(com, cut_all=False)
-            word = [com_sep for com_sep in coms_sep]
-            # 同一個人做的事，儲忖在同一dict比較能體現效果 
-            articles.append({'user':user, 
-                             'score':score, 
-                             'comment':(comment.text).replace('\r\n', ''),
-                             'comment_sep':word,
-                             'com':coms_sep}) 
-# print (comment.text) 
-    url = change_page(url) # 頁面資訊分析完後，換頁 
-    # 依次打印每個頁面 
-    for i in (articles): 
-        print('評論：\n', i['comment'])
-        print('切割後的評論：\n', i['comment_sep'])
-        print('111', i['com'])
+    soup = parse_url(url)
+
+    comment_area = soup.select('ul[class="usercom_list"]')[0]
+    comment_blocks = comment_area.select('li')
+    for comment_block in comment_blocks:
+        comment_block = comment_block.form
+        user = comment_block.select('.user_id')[0].text
+        score = int(comment_block.select('input[name~=score]')[0]['value'])
+        comment = comment_block.select('span')[-1].text.replace('\r\n', '')
+        words = jieba.cut(comment, cut_all=False)
+        word = [word for word in words]
+        comment_sep = '/'.join(word)
+#        print(user, '\n', score, '\n', comment, '\n')
+        
+        item.append([user, score, comment, comment_sep])
+#        item.append({'name' : user,
+#                     'score' : score,
+#                     'comment' : comment,
+#                     'com_sep' : comment_sep}) 
+    url = change_page(url)
+for i in item:
+    print(i[2], '\n', i[3])
+    print('\n')
+        
